8999332e8b2e59b73287defe25ccd53b57d1b761
Pita <salahbenalmadena@gmail.com>
2025-07-30 14:52:56 +0200

    Files update, everything working fine.

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const songs = [
     ["His palms are sweaty, knees weak, arms are heavy", "There\'s vomit on his sweater already, mom\'s spaghetti", "He\'s nervous, but on the surface, he looks calm and ready"],
     ["You float like a feather", "In a beautiful world", "I wish I was special"]];
 
-//This variable will sort the songs to be ordered in a random way without repeating themselves.
+
+
+
+//Function in html.
+
+
+
+function imprimirMensaje() {
+    //This variable will sort the songs to be ordered in a random way without repeating themselves.
 const unorderedSongs = songs.sort(() => Math.random() - 0.5)
 
 //This variable choses a random index of each song, giving us a different part of each song joined in an array and line breaked.
-const frases = unorderedSongs.map(group => group[Math.floor(Math.random() * group.length)]).join('\n');
+const frases = unorderedSongs.map(group => group[Math.floor(Math.random() * group.length)]).join('<br><br>');
 //We print them.
-console.log(frases);
+      const box = document.getElementById('box');
+      box.innerHTML = '';
+
+      const nuevoParrafo = document.createElement('p');
+      nuevoParrafo.innerHTML = frases;
+      box.appendChild(nuevoParrafo);
+console.log('Button clicked, message added.');
+    }
